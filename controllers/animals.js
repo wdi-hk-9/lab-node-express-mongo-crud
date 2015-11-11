d6774196c92c6ee187401c7770214ba80cb2fed9
Charlotte Kwan <kwan.charlotte.cl@gmail.com>
2015-11-12 02:28:11 +0800

    fixed indentations and routes

--- controllers/animals.js
+++ controllers/animals.js
@@ -2,20 +2,24 @@ var express    = require('express'),
     router     = express.Router(),
     bodyParser = require('body-parser'),
       // https://www.npmjs.com/package/body-parser
-    methodOverride = require('method-override');
+    methodOverride = require('method-override'),
       // https://www.npmjs.com/package/method-override
-
-var Animal = require('./models/user');
+    mongoose = require('mongoose'),
+    Animal = require('../models/animals');
 
 // http://nano:3000/animals
 router.route('/')
   // INDEX
   .get(function(req, res, next) {
     Animal.find({}, function(err, animals){
-      if (err) console.log(err);
-      res.render('index',{
-        animal: animal
-      });
+      if (!err) {
+        console.log(err);
+        res.render('index',{
+        animals: animals
+        });
+      } else {
+        return console.log(err);
+      }
     });
   });
   // CREATE
