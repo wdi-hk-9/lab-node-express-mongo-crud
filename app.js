af584e4faec0875102fe7c313f1e4885eb580c2e
franxyzxyz <franxyzxyxz@gmail.com>
2015-11-12 02:20:54 +0800

    node-express-mongo

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var expressLayouts = require('express-ejs-layouts');
 
+var ObjectId = require('mongodb').ObjectID;
+
 var app = express();
 var router = express.Router();
 
@@ -16,8 +18,8 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('views', path.join(__dirname, 'views'));
-app.use(expressLayouts);
-app.engine('ejs', require('ejs').renderFile);
+// app.use(expressLayouts);
+// app.engine('ejs', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 // development error handler
@@ -37,3 +39,56 @@ console.log("Server Started");
 
 // ############ YOU CAN ADD YOUR CODE BELOW ########
 // ###### HAPPY CODING  :) #########################
+//
+var Animal = require('./models/animal');
+
+// Index
+router.get('/', function(req,res){
+  Animal.find({}, function(err,animals){
+    res.render('index',{animals});
+  })
+});
+// Show
+router.get('/:id', function(req,res){
+  Animal.findById(req.params.id,function(err,animal){
+    res.render('show',{animal});
+  });
+});
+
+// Update
+router.post('/:id', function(req,res){
+  Animal.findById(req.params.id,function(err,animal){
+    switch(animal.status){
+      case "Abandon":
+        animal.status = "Adopt";
+        animal.save(function(err){
+          if (err) console.log(err);
+          res.redirect('/animals/');
+        });
+        break;
+      case "Adopt":
+        animal.status = "Abandon";
+        animal.save(function(err){
+          if (err) console.log(err);
+          res.redirect('/animals/');
+        });
+        break;
+      }
+  });
+});
+
+// New
+router.post('/', function(req,res){
+  var newPetBase = req.body;
+  newPetBase.status = "Abandon";
+  var newPet = Animal(newPetBase);
+  console.log(newPet);
+  newPet.save(function(err){
+    if (err) console.log(err);
+    res.redirect('/animals')
+  })
+});
+
+// Route middleware
+app.use('/animals', router);
+
